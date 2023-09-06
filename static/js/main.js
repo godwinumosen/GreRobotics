6b525264674980ec5cc3ce40c1a373738289bb2f
GodwinCoder568 <121322124+GodwinCoder568@users.noreply.github.com>
2023-09-06 11:31:46 -0700

    created a mobile navbar toggle

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: eBusiness
-* Updated: Jul 27 2023 with Bootstrap v5.3.1
-* Template URL: https://bootstrapmade.com/ebusiness-bootstrap-corporate-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
