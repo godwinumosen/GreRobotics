0ee312c071a32070ca58bacc46c7a23348e628c7
GodwinCoder568 <121322124+GodwinCoder568@users.noreply.github.com>
2023-09-06 10:24:13 -0700

    created a base.html in template

--- robotics/views.py
+++ robotics/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render,redirect
 
 # Create your views here.
 
+
 def index (request) :
-    return render (request, 'robotics/index.html')
+    return render (request, 'robotics/index.html',{})
+
+def base (request):
+    return render (request, 'base.html',{})
