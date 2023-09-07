c0f43c583285a6b134a6c24cd4675ee120ce18bb
GodwinCoder568 <121322124+GodwinCoder568@users.noreply.github.com>
2023-09-07 04:26:32 -0700

    created a mobile navbar toggle

--- robotics/admin.py
+++ robotics/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import GREROBOTICSMODEL
+
+
+class GREROBOTICSMODELADMIN (admin.ModelAdmin):
+    list_display = ['title','content','slug','author']
+    admin.site.register (GREROBOTICSMODEL)
--- robotics/models.py
+++ robotics/models.py
@@ -1,6 +1,29 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.urls import reverse
+from datetime import datetime, date
 # Create your models here.
 
-'''class GREROBOTICSMODEL (models.Model):
-    title ='''
+class GREROBOTICSMODEL (models.Model):
+    title = models.CharField (max_length=500)
+    content = models.TextField ()
+    slug = models.SlugField (max_length=200)
+    img = models.ImageField ( upload_to= 'image/')
+    publish_date = models.DateTimeField (auto_now_add= True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    class Meta:
+        ordering =['-publish_date']
+    
+    def __str__(self):
+        return self.title
+    
+     
+    def get_absolute_url(self):
+        return reverse ('detail', args=(str(self.id)))
+    
+    @property
+    def image_url(self):
+        if self.img and hasattr(self.img, 'url'):
+            return self.img.url
+    
